9b3d97c22a23aab8f299f11162e08e5c1580289a
TheProfezzzor <TheProfezzzor@users.noreply.github.com>
2021-03-12 15:33:07 -0500

    Remove pathfinding members from PlayScene.

--- src/PlayScene.h
+++ src/PlayScene.h
@@ -4,13 +4,11 @@
 
 #include "Scene.h"
 #include "Button.h"
-#include "Heuristic.h"
 #include "Label.h"
 #include "Obstacle.h"
 #include "ship.h"
 #include "SpaceShip.h"
 #include "Target.h"
-#include "Tile.h"
 
 class PlayScene : public Scene
 {
@@ -35,32 +33,6 @@ private:
 	Target* m_pTarget;
 	Ship* m_pShip;
 
-	// Pathfinding functions and objects
-	void m_buildGrid();
-	void m_computeTileCosts();
-	void m_findShortestPath();
-	void m_displayPathList();
-	
-	void m_setGridEnabled(bool state);
-	bool m_getGridEnabled() const;
-	std::vector<Tile*> m_pGrid;
-
-	// convenience functions
-	Tile* m_getTile(int col, int row);
-	Tile* m_getTile(glm::vec2 grid_position);
-
-	// heuristic
-	Heuristic currentHeuristic;
-
-	// open, closed and path lists
-	std::vector<Tile*> m_pOpenList;
-	std::vector<Tile*> m_pClosedList;
-	std::vector<Tile*> m_pPathList;
-
-	void m_moveShip();
-	int moveCounter = 0;
-	bool m_shipIsMoving = false;
-	
 };
 
 
